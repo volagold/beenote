8d0c266c81c3e39b12aa76cb7a208167980bb515
playhousehosting <126509681+playhousehosting@users.noreply.github.com>
2024-12-08 18:17:26 -0800

    Update next.config.js with additional image domains

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['beenote.vercel.app'],
+    domains: [
+      'beenote.vercel.app',
+      'raw.githubusercontent.com',
+      'github.com',
+      'avatars.githubusercontent.com'
+    ],
   },
   experimental: {
     serverActions: true,
