b0442dc18789a81b1a8c818d466a4925ef9a616c
playhousehosting <126509681+playhousehosting@users.noreply.github.com>
2024-12-08 18:34:43 -0800

    Update Next.js config with auth redirects

--- next.config.js
+++ next.config.js
@@ -12,7 +12,22 @@ const nextConfig = {
   },
   experimental: {
     serverActions: true,
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/u',
+        has: [
+          {
+            type: 'header',
+            key: 'x-prerender-revalidate',
+          },
+        ],
+        destination: '/login',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
